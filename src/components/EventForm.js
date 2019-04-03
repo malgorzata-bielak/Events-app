e084f90ca700bd67b5eecf8161261251db50391d
Malgorzata Bielak <mgw.bielak@gmail.com>
2019-04-03 21:14:03 +0200

    Change webpack configuration

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,30 +1,85 @@
 import React from "react";
 import { DateRangePicker } from "react-dates";
+import moment from "moment";
 
-const EventForm = () => (
-  <div>
-    <input placeholder="Title" />
-    <input placeholder="Description" />
-    <input placeholder="Organisator" />
-    <select>
-      <option>Select city</option>
-      <option>Cracow</option>
-      <option>Wroclaw</option>
-      <option>Warsaw</option>
-      <option>Poznan</option>
-      <option>Gdansk</option>
-    </select>
-    <select>
-      <option>Select category</option>
-      <option>Music</option>
-      <option>Arts</option>
-      <option>Business</option>
-      <option>Sport</option>
-      <option>Food</option>
-    </select>
-    {/* <DateRangePicker /> */}
-    <input type="file" accept="image/*" />
-  </div>
-);
+export default class EventForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-export default EventForm;
+    this.state = {
+      title: "",
+      description: "",
+      organisator: "",
+      city: "",
+      category: "",
+      image: false,
+      createdAt: moment(),
+      date: 0,
+      id: "",
+    };
+  }
+
+  onTitleChange = e => {
+    const title = e.target.value;
+    this.setState(() => ({ title }));
+    console.log(this.state.title);
+  };
+
+  onDescriptionChange = e => {
+    const description = e.target.value;
+    this.setState(() => ({ description }));
+    console.log(this.state.description);
+  };
+
+  onOrganisatorChange = e => {
+    const organisator = e.target.value;
+    this.setState(() => ({ organisator }));
+    console.log(this.state.organisator);
+  };
+
+  onCityChange = e => {
+    const city = e.target.value;
+    this.setState(() => ({ city }));
+    console.log(this.state.city);
+  };
+
+  onCategoryChange = e => {
+    const category = e.target.value;
+    this.setState(() => ({ category }));
+    console.log(this.state.category);
+  };
+
+  onImageChange = e => {
+    const image = e.target.value;
+    this.setState(() => ({ image }));
+    console.log(this.state.image);
+  };
+
+  render() {
+    return (
+      <div>
+        <input placeholder="Title" onChange={this.onTitleChange} />
+        <input placeholder="Description" onChange={this.onDescriptionChange} />
+        <input placeholder="Organisator" onChange={this.onOrganisatorChange} />
+        <select value={this.state.city} onChange={this.onCityChange}>
+          <option value="">Select city</option>
+          <option value="cracow">Cracow</option>
+          <option value="wroclaw">Wroclaw</option>
+          <option value="warsaw">Warsaw</option>
+          <option value="poznan">Poznan</option>
+          <option value="gdansk">Gdansk</option>
+        </select>
+        <select value={this.state.category} onChange={this.onCategoryChange}>
+          <option value="">Select category</option>
+          <option value="music">Music</option>
+          <option value="arts">Arts</option>
+          <option value="business">Business</option>
+          <option value="sport">Sport</option>
+          <option value="food">Food</option>
+        </select>
+        {/* <DateRangePicker /> */}
+        <input type="file" accept="image/*" onChange={this.onImageChange} />
+      </div>
+    );
+  }
+}
