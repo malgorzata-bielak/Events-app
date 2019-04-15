ae9b4dbb38eb55dbe9e783e5e05fd42a23643b2d
Malgorzata Bielak <mgw.bielak@gmail.com>
2019-04-15 11:03:02 +0200

    Make header component link to dashboard

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Header = () => <h1>Event App</h1>;
+const Header = () => (
+  <Link to="/">
+    <h1>Event App</h1>
+  </Link>
+);
 
 export default Header;
